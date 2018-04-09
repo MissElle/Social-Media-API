62a5866952b98895f885d16ca056cd942fbfe8ce
MissElle <elli.g.pope@gmail.com>
2018-04-08 22:33:04 -0700

    setting up a get by user

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = function(){
     var feed = new Instafeed({
-        get: '',
-        tagName: '',
+        get: 'user',
+        tagName: 'boobitters',
         clientId: '	917566a744474d8a93a311662aa5be2f'
     });
     feed.run();
