41cd4d3ffdf0db918bdec8e7180125037b05e9f3
MissElle <elli.g.pope@gmail.com>
2018-04-09 17:35:06 -0700

    removed instafeed.js and it's associated code/markup

--- script.js
+++
@@ -1,8 +0,0 @@
-window.onload = function(){
-    var feed = new Instafeed({
-        get: 'user',
-        tagName: 'boobitters',
-        clientId: '	917566a744474d8a93a311662aa5be2f'
-    });
-    feed.run();
-}
